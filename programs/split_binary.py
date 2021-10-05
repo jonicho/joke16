c6ab1836986072c09b809d3035922460335a5ccb
Jonas Richard Keller <jonas.r.keller@gmail.com>
2021-10-05 20:16:02 +0200

    Fix wrong file order in split_binary.py

--- programs/split_binary.py
+++ programs/split_binary.py
@@ -3,19 +3,19 @@ import os
 filename = os.path.splitext(sys.argv[1])[0]
 extension = os.path.splitext(sys.argv[1])[1]
 f = open(sys.argv[1], "rb")
-f_1 = open(filename+".part1"+extension, "wb")
-f_2 = open(filename+".part2"+extension, "wb")
+f_0 = open(filename+".0"+extension, "wb")
+f_1 = open(filename+".1"+extension, "wb")
 even = True
 while (byte := f.read(1)):
     if even:
         f_1.write(byte)
     else:
-        f_2.write(byte)
+        f_0.write(byte)
     even = not even
 
 if not even:
     print("warning: file had an odd number of bytes!")
 
 f.close()
+f_0.close()
 f_1.close()
-f_2.close()
